d88042eeb5a6b6216d0712e10e72e941c04deaf0
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-02-12 16:22:12 +0900

    Fix: CD > Auto() | Can be specified remote and branch.

--- CD.class.php
+++ CD.class.php
@@ -47,21 +47,48 @@ class CD implements IF_UNIT
 	 */
 	static function Auto()
 	{
+		//	Init
+		$branches = [];
+
 		//	...
-		$prefix = '.ci_commit_id_';
-		foreach( glob("{$prefix}*") as $file ){
-			$branch_name      = substr($file, strlen($prefix));
-			$commit_id_saved  = file_get_contents($file);
+		$remote = OP()->Request('remote') ?? 'origin';
+		$branch = OP()->Request('branch');
+
+		//	Get the specified branch.
+		if( $branch ){
+			if( $branch === '\*' ){
+				/*
+				//	Generate inspected branches by .ci_commit_id files.
+				$prefix = '.ci_commit_id_';
+				foreach( glob("{$prefix}*") as $file ){
+					$branches[] = substr($file, strlen($prefix));
+				}
+				*/
+				//	Generate branches by instanced branch names.
+				$branches = self::Git()->Branches();
+			}else{
+				//	Generate branches by specified branch name.
+				$branches[] = $branch;
+			}
+		}else{
+			//	Generate branches by current branch name.
+			$branches[] = self::Git()->CurrentBranch();
+		}
+
+		//	Execute each branch name.
+		foreach( $branches as $branch_name ){
+			$commit_id_file   = ".ci_commit_id_{$branch_name}";
+			$commit_id_saved  = file_get_contents($commit_id_file);
 			$commit_id_branch = self::Git()->CommitID($branch_name);
-			D($file, $branch_name, $commit_id_saved, $commit_id_branch);
+			D($remote, $branch_name, $commit_id_file, $commit_id_saved, $commit_id_branch);
 
 			//	...
 			if( $commit_id_saved !== $commit_id_branch ){
-				throw new Exception("Does not match commit id. ({$file}={$commit_id_saved}, {$branch_name}={$commit_id_branch})");
+				throw new Exception("Does not match commit id. ({$commit_id_file}={$commit_id_saved}, {$branch_name}={$commit_id_branch})");
 			}
 
 			//	...
-			self::Git()->Push($branch_name);
+			self::Git()->Push($remote, $branch_name);
 		}
 	}
 
