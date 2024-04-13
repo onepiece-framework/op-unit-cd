c2ceda1edd21af5fa1eafc544b4082452b73e5aa
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-04-13 11:55:54 +0900

    New: CD_2024.trait.php

---
+++ CD_2024.trait.php
@@ -0,0 +1,41 @@
+<?php
+/** op-unit-cd:/CD_2024.trait.php
+ *
+ * @created    2024-04-12
+ * @version    1.0
+ * @package    op-unit-cd
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\CD;
+
+/** use
+ *
+ */
+
+/** include
+ *
+ */
+require_once(__DIR__.'/function/Display.php');
+require_once(__DIR__.'/function/PathList.php');
+
+/** CD_2024
+ *
+ * @created    2024-04-12
+ * @version    1.0
+ * @package    op-unit-cd
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait CD_2024
+{
+}
