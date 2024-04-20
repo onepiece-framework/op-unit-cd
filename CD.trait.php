c34c70e3abc0805448bd113d00c6e1b38f0ae392
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-04-21 01:00:13 +0900

    New: CD :: _CheckGitCommitId()

--- CD.trait.php
+++ CD.trait.php
@@ -43,4 +43,52 @@ trait CD
 	{
 		return OP()->Unit('Git');
 	}
+
+	/** Check if the current branch is inspected.
+	 *
+	 * @created    2024-04-21
+	 * @return     bool
+	 */
+	static private function _CheckGitCommitId() : bool
+	{
+		//	...
+		if(!$meta_path = OP()->MetaPath( getcwd() ) ){
+			OP()->Notice("MetaPath is return false.");
+			return false;
+		}
+
+		//	...
+		if(!$branch = OP()->Request('branch') ?? self::Git()->Branch()->Current() ){
+			OP()->Notice("Branch name is empty.");
+			return false;
+		}
+
+		//	Get saved commit ID from file.
+		$file_name = self::CI()->GenerateFilename($branch);
+		if(!file_exists($file_name) ){
+			OP()->Notice("Does not found this file. ($meta_path, $file_name)");
+			return false;
+		}
+
+		//	...
+		if(!$commit_id_saved = file_get_contents($file_name) ){
+			OP()->Notice("Does not saved commit id. ($file_name)");
+			return false;
+		}
+
+		//	Checking correct commit ID.
+		if(!$commit_id = self::Git()->CommitID($branch) ){
+			OP()->Notice("Empty commit id. ($branch)");
+			return false;
+		}
+
+		//	...
+		if( $commit_id_saved !== $commit_id ){
+			OP()->Notice("Does not match commit id. ({$meta_path}, {$file_name}={$commit_id_saved}, {$branch}={$commit_id})");
+			return false;
+		}
+
+		//	...
+		return true;
+	}
 }
