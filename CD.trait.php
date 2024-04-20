476eadde557b0a91e40937d56c0b6fce8fded7c8
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-04-21 00:59:03 +0900

    New: CD.trait.php

---
+++ CD.trait.php
@@ -0,0 +1,27 @@
+<?php
+/** op-unit-cd:/CD.trait.php
+ *
+ * @created    2024-04-21
+ * @version    1.0
+ * @package    op-unit-cd
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\CD;
+
+/** use
+ *
+ */
+
+trait CD
+{
+}
