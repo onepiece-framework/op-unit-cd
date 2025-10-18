06aa0854e6eed8804ffc71d67e3aca2ab41ac89e
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-18 15:54:03 +0900

    New: ci > CD > PushGitRepository.php

---
+++ ci/CD/PushGitRepository.php
@@ -0,0 +1,29 @@
+<?php
+/** op-unit-cd:/ci/CD/PushGitRepository.php
+ *
+ * @created    2025-10-18
+ * @version    1.0
+ * @package    op-unit-cd
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = 'PushGitRepository';
+
+//	...
+$args   =  null;
+$result =  null;
+$ci->Set($method, $result, $args);
