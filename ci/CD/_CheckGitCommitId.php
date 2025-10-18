351deda4898b9150e49b25272cdbbff9d0ed1f58
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-18 15:53:36 +0900

    New: ci > CD > _CheckGitCommitId.php

---
+++ ci/CD/_CheckGitCommitId.php
@@ -0,0 +1,29 @@
+<?php
+/** op-unit-cd:/ci/CD/_CheckGitCommitId.php
+ *
+ * @created    2025-10-18
+ * @version    1.0
+ * @package    op-unit-cd
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = '_CheckGitCommitId';
+
+//	...
+$args   =  null;
+$result =  true;
+$ci->Set($method, $result, $args);
