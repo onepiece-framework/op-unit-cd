onepiece-framework/op-unit-ci

New: Empty.class.php

Empty.class.php
@@ -1,0 +1,39 @@
+<?php
+/** op-unit-empty:/Empty.class.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-empty
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** use
+ *
+ */
+use OP\IF_UNIT;
+use OP\OP_CORE;
+use OP\OP_CI;
+
+/** Empty
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-empty
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+class _Empty implements IF_UNIT
+{
+	use OP_CORE, OP_CI;
+}

onepiece-framework/op-unit-cd

Empty.class.php
@@ -1,0 +1,39 @@
+<?php
+/** op-unit-empty:/Empty.class.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-empty
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** use
+ *
+ */
+use OP\IF_UNIT;
+use OP\OP_CORE;
+use OP\OP_CI;
+
+/** Empty
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-empty
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+class Git implements IF_UNIT
+{
+	use OP_CORE, OP_CI;
+}